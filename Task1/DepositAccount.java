d945dcf9ebc715ee4d7852587dd9cd460d9ce031
frufrok <frufrok@gmail.com>
2023-12-14 22:46:06 +0300

    Добавлено переопределение метода putAmount для DepositAccount, чтобы обновлялась дата последнего пополнения. Сделан метод для перевода времени на 1 месяц назад, чтобы продемонстировать работоспособность DepositAccount.

--- Task1/DepositAccount.java
+++ Task1/DepositAccount.java
@@ -26,20 +26,38 @@ public class DepositAccount extends Account {
         value = Account.round(value);
         if (value > 0) {
             long elapsed = ChronoUnit.MONTHS.between(lastPutDateTime, LocalDateTime.now());
-            if (elapsed >=1) {
-                takeAmount(value);
+            if (elapsed >= 1) {
+                super.takeAmount(value);
+            } else {
+                this.addHistoryRecord(String.format(
+                        "Неудачная попытка снятия средств на сумму: %.2f. Дата последнего пополнения: %s.", value,
+                        this.lastPutDateTime));
             }
-            else {
-                this.addHistoryRecord(String.format("Неудачная попытка снятия средств на сумму: %.2f. Дата последнего пополнения: %s.", value, this.lastPutDateTime));
-            }
+        } else {
+            this.addHistoryRecord(
+                    String.format("Неудачная попытка снятия средств на сумму: %.2f. Текущий баланс: %.2f.", value,
+                            this.getAmountProtected()));
         }
-        else {
-            this.addHistoryRecord(String.format("Неудачная попытка снятия средств на сумму: %.2f. Текущий баланс: %.2f.", value, this.getAmountProtected()));
+    }
+    
+    @Override
+    public void putAmount(double value) {
+        value = round(value);
+        if (value > 0) {
+            super.putAmount(value);
+            this.lastPutDateTime = LocalDateTime.now();
+        } else {
+            addHistoryRecord(String.format("Неудачная попытка зачисления средств на сумму: %.2f. Текущий баланс: %.2f.",
+                    value, getAmountProtected()));
         }
     }
 
     @Override
     public String toString() {
         return String.format("Deposit account ID: %s", this.getID());
+    }
+
+    public void turnBackInTimeDemo() {
+        this.lastPutDateTime = LocalDateTime.now().minusMonths(1);
     }
 }
--- Task1/Program.java
+++ Task1/Program.java
@@ -1,13 +1,23 @@
 package Task1;
 
+import java.util.concurrent.TimeUnit;
+
 public class Program {
     public static void main(String[] args) {
         Account acc1 = new Account(1, 90);
         checkAccount(acc1);
         Account acc2 = new CreditAccount(2, 100);
         checkAccount(acc2);
-        Account acc3 = new DepositAccount(3, 110);
+        DepositAccount acc3 = new DepositAccount(3, 110);
         checkAccount(acc3);
+
+        System.out.println("Переводим время на 1 месяц назад для последнего пополнения.");
+        acc3.turnBackInTimeDemo();
+        System.out.println("Время последнего пополнения:");
+        System.out.println(acc3.getLastPutDateTime());
+        System.out.println("Снимаем средства.");
+        acc3.takeAmount(50);
+        printHistory(acc3);
     }
     
     private static void printHistory(Account account) {
@@ -24,6 +34,13 @@ public class Program {
         account.putAmount(-10);
         account.putAmount(0);
         account.putAmount(10);
+        // Для проверки времени последнего зачисления.
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         account.putAmount(10.123456);
 
         account.takeAmount(-10);
