f7db1555e16b6bb2cd87e12d3c37c16d64c733c4
frufrok <frufrok@gmail.com>
2023-12-18 20:26:34 +0300

    Выполнено домашнее задание к уроку 2.

---
+++ Task2/Program.java
@@ -0,0 +1,31 @@
+package Task2;
+
+public class Program {
+    public static void main(String[] args) {
+        Human man = new Human("Азат");
+        print(man.toString());
+        print("Все уровни:");
+        System.out.print(man.getLevelsExperiences());
+        print("");
+
+        BaseEvent event1 = new BaseEvent("Домашнее задание сдано", 15);
+        print(event1.toString());
+        ExperienceChangedHandler.handleExperienceChanged(man, event1);
+        print(man.toString());
+
+        BaseEvent event2 = new BaseEvent("Завалена промежуточная аттестация", -150);
+        print(event2.toString());
+        ExperienceChangedHandler.handleExperienceChanged(man, event2);
+        print(man.toString());
+
+        BaseEvent event3 = new BaseEvent("Защита диплома успешно пройдена", 500);
+        print(event3.toString());
+        ExperienceChangedHandler.handleExperienceChanged(man, event3);
+        print(man.toString());
+
+
+    }
+    public static void print(String txt) {
+        System.out.println(txt);
+    }
+}
---
+++ Task2/Unit.java
@@ -0,0 +1,64 @@
+package Task2;
+
+import java.util.HashMap;
+
+public abstract class Unit implements LevelChangeable {
+    private String name;
+    private int currentLevel;
+    private int currentExperience;
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getCurrentLevel() {
+        return this.currentLevel;
+    }
+    public int getCurrentExperience() {
+        return this.currentExperience;
+    }
+    abstract public int getCurrentLevelExperience();
+    abstract public HashMap<Integer,Integer> getLevelsExperiences();
+    public void changeExperience(int value) {
+        int newValue = this.currentExperience + value;
+        int curLevelExp = this.getCurrentLevelExperience();
+        if (newValue >= 0) {
+            if (curLevelExp >= 0 && newValue >= curLevelExp) {
+                this.currentLevel += 1;
+                this.changeExperience(newValue - curLevelExp);
+            }
+            else {
+                this.currentExperience = newValue;
+            }
+        }
+        else {
+            if (this.currentLevel > 1) {
+                this.currentLevel -= 1;
+                this.changeExperience(newValue + this.getCurrentExperience());
+            }
+            else {
+                this.currentExperience = 0;
+            }
+        }
+    }
+
+    public Unit(String name) {
+        this.name = name;
+        this.currentLevel = 1;
+        this.currentExperience = 0;
+    }
+
+    @Override
+    public String toString() {
+        int currentLevelExperience = this.getCurrentLevelExperience();
+        if (currentLevelExperience >= 0) {
+            return String.format("Юнит %s, текущий уровень: %d, текущий опыт: %d, следующий уровень по достижении %d",
+                    this.name, this.currentLevel, this.currentExperience, this.getCurrentLevelExperience());
+        }
+        else {
+            return String.format("Юнит %s, текущий уровень: %d, текущий опыт: %d, полностью прокачан.",
+                    this.name, this.currentLevel, this.currentExperience);
+        }
+    }
+}
